41e6fab5d3d13dff1047d84684035e786f915f9d
Goodson Nyasha Mutongi <nyasha@escrowgroup.org>
2020-03-02 09:35:37 +0200

    adding a new RootObject

---
+++ DatabaseManipulatorTest/UpdatedDataTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DatabaseManipulatorTest
+{
+    [TestClass]
+    public class UpdatedDataTest
+    {
+        [TestMethod]
+        public void Update_Test()
+        {
+            int expectedAge = 30;
+
+            var actual = new DatabaseManipulator.RootObject();
+            actual.ConnectionString = "";
+            
+
+        }
+    }
+}
